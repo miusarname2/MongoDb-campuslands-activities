1bbc9ca7bc2ee28adfcb97b59963f7d5bace9c6c
miusarname <oscarm.alvarezg@gmail.com>
2023-08-09 21:03:29 -0500

    feat: :tada: Se creo el proyecto

---
+++ db/atlas.js
@@ -0,0 +1,13 @@
+dotenv.config("../");
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+export async function con() {
+  try {
+      const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.ap9ecpy.mongodb.net/${process.env.ATLAS_DB}`;
+      const options = { useNewUrlParser: true, useUnifiedTopology: true };
+      const client = await MongoClient.connect(uri, options);
+      return client.db();
+  } catch (error) {
+      return { status: 500, message: error.message };
+  }
+}
---
+++ routers/storage/errorHandler.ts
@@ -0,0 +1,121 @@
+export class ErrorHandler {
+  errorHandle: Error | Object | Array<string | Object>;
+
+  constructor(errorToHandle: any) {
+    this.errorHandle = errorToHandle.errInfo.details.schemaRulesNotSatisfied;
+  }
+
+  private errorHandlers = {
+    ECONNREFUSED: {
+      status: 503,
+      message:
+        "No se pudo establecer conexión con el servidor de base de datos.",
+    },
+    ENETUNREACH: {
+      status: 503,
+      message:
+        "No se puede alcanzar la red que aloja el servidor de base de datos.",
+    },
+    AuthenticationFailed: {
+      status: 401,
+      message: "Error de autenticación al acceder a la base de datos.",
+    },
+    SyntaxError: {
+      status: 400,
+      message: "Error de sintaxis en una consulta a la base de datos.",
+    },
+    ValidationError: {
+      status: 422,
+      message:
+        "Los datos proporcionados no cumplen con las reglas de validación.",
+    },
+    DocumentExpired: {
+      status: 410,
+      message: "El documento al que intenta acceder ha expirado.",
+    },
+    ServerClosed: {
+      status: 503,
+      message:
+        "La conexión con el servidor de base de datos se cerró inesperadamente.",
+    },
+    MongoTimeoutError: {
+      status: 504,
+      message:
+        "Tiempo de espera agotado al realizar una operación en la base de datos.",
+    },
+    BSONError: {
+      status: 400,
+      message: "Error relacionado con la codificación y decodificación BSON.",
+    },
+    properties: {
+      status: 406,
+      message:
+        "Alguna de las propiedades que estas enviando estan de forma incorrecta, consulta con el admin 😎",
+    },
+    additionalProperties: {
+      status: 413,
+      message: "Tienes demasiadas propiedades, consulta con el admin 😎",
+    },
+    default: {
+      status: 400,
+      message:
+        "Tienes algo mal en el envio, consulta con el admin 😎 o la documentacion 📜",
+    },
+  };
+
+  public get handerErrorSucess(): Object {
+    let responses: Object | string;
+    if (Array.isArray(this.errorHandle)) {
+      this.errorHandle.forEach((item) => {
+        if (typeof item === "object" && item !== null) {
+          console.log(item.operatorName);
+          switch (item.operatorName) {
+            case "properties":
+              responses = this.errorHandlers.properties;
+              break;
+            case "additionalProperties":
+              responses = this.errorHandlers.additionalProperties;
+              break;
+
+            case "ECONNREFUSED":
+              responses = this.errorHandlers.ECONNREFUSED;
+              break;
+
+            case "ENETUNREACH":
+              responses = this.errorHandlers.ENETUNREACH;
+              break;
+
+            case "AuthenticationFailed":
+              responses = this.errorHandlers.AuthenticationFailed;
+              break;
+
+            case "SyntaxError":
+              responses = this.errorHandlers.SyntaxError;
+              break;
+
+            case "ValidationError":
+              responses = this.errorHandlers.ValidationError;
+              break;
+
+            case "ServerClosed":
+              responses = this.errorHandlers.ServerClosed;
+              break;
+
+            case "MongoTimeoutError":
+              responses = this.errorHandlers.MongoTimeoutError;
+              break;
+
+            case "BSONError":
+              responses = this.errorHandlers.BSONError;
+              break;
+
+            default:
+              responses = this.errorHandlers.default;
+              break;
+          }
+        }
+      });
+    }
+    return responses;
+  }
+}
