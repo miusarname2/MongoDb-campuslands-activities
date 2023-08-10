ef3ed2a163739bfb0906d918b3eeb32dafcf7270
miusarname <oscarm.alvarezg@gmail.com>
2023-08-09 21:22:25 -0500

    feat: :sparkles: leve modificacion en el atlas

--- db/atlas.js
+++ db/atlas.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv'
 export async function con() {
   try {
+    
       const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.ap9ecpy.mongodb.net/${process.env.ATLAS_DB}`;
       const options = { useNewUrlParser: true, useUnifiedTopology: true };
       const client = await MongoClient.connect(uri, options);
